486acb2a4be09a12061c8b5756281418c494110a
kalidas120799 <s.kalidas120799@gmail.com>
2024-04-27 18:44:15 +0530

    Changed ENV key

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 export const GET = () => {
     return NextResponse.json({
         name: 'media-downloader',
-        version: "1.0.0"
+        version: process.env.APP_VERSION
     })
 }
--- src/helpers/web-agent.js
+++ src/helpers/web-agent.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.APP_BASE_URL || 'http://localhost:3000';
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_APP_BASE_URL | 'http://localhost:3000';
 
 export const webAgent = axios;
